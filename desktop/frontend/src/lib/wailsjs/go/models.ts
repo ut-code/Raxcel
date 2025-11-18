3a9d9421f27619d6a535cfd68cd686b11b865274
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-11-18 18:15:04 +0800

    move to the correct place

--- desktop/frontend/src/lib/wailsjs/go/models.ts
+++ desktop/frontend/src/lib/wailsjs/go/models.ts
@@ -1,64 +1,67 @@
 export namespace main {
-  export class ChatResult {
-    ok: boolean;
-    message: string;
+	
+	export class ChatResult {
+	    ok: boolean;
+	    message: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new ChatResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.ok = source["ok"];
+	        this.message = source["message"];
+	    }
+	}
+	export class CheckResult {
+	    ok: boolean;
+	    userId?: string;
+	    error?: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new CheckResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.ok = source["ok"];
+	        this.userId = source["userId"];
+	        this.error = source["error"];
+	    }
+	}
+	export class LoginResult {
+	    ok: boolean;
+	    message: string;
+	    token?: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new LoginResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.ok = source["ok"];
+	        this.message = source["message"];
+	        this.token = source["token"];
+	    }
+	}
+	export class RegisterResult {
+	    ok: boolean;
+	    message: string;
+	    userId?: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new RegisterResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.ok = source["ok"];
+	        this.message = source["message"];
+	        this.userId = source["userId"];
+	    }
+	}
 
-    static createFrom(source: any = {}) {
-      return new ChatResult(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.ok = source["ok"];
-      this.message = source["message"];
-    }
-  }
-  export class CheckResult {
-    ok: boolean;
-    userId?: string;
-    error?: string;
-
-    static createFrom(source: any = {}) {
-      return new CheckResult(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.ok = source["ok"];
-      this.userId = source["userId"];
-      this.error = source["error"];
-    }
-  }
-  export class LoginResult {
-    ok: boolean;
-    message: string;
-    token?: string;
-
-    static createFrom(source: any = {}) {
-      return new LoginResult(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.ok = source["ok"];
-      this.message = source["message"];
-      this.token = source["token"];
-    }
-  }
-  export class RegisterResult {
-    ok: boolean;
-    message: string;
-    userId?: string;
-
-    static createFrom(source: any = {}) {
-      return new RegisterResult(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.ok = source["ok"];
-      this.message = source["message"];
-      this.userId = source["userId"];
-    }
-  }
 }
+
